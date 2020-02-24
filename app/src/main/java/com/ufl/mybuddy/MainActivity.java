361bb0dd62ad9af07a0f1ef1411994a34b825820
kevinbnaughton <kevinbnaughton@gmail.com>
2020-02-23 21:47:24 -0500

    Drawer is a bit broken

--- app/src/main/java/com/ufl/mybuddy/MainActivity.java
+++ app/src/main/java/com/ufl/mybuddy/MainActivity.java
@@ -1,14 +1,30 @@
 package com.ufl.mybuddy;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
@@ -18,6 +34,9 @@ import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,11 +48,25 @@ public class MainActivity extends AppCompatActivity {
     // Firebase
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    //What kind of account?
+    private String mAccountType = "default";
 
     // AR Sceneform
     private ArFragment arFragment;
     private ModelRenderable mCorgi;
 
+    private FloatingActionButton mVoiceFab;
+
+    // Navigation Bar
+    private NavigationView mNavigationView;
+    private CircleImageView mNavProfileImage;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private Toolbar mToolbar;
+    private TextView mProfileName;
+
+    // Google Sign in
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        buttonInit();
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
 
         // When you build a Renderable, Sceneform loads its resources in the background while returning
@@ -93,7 +127,6 @@ public class MainActivity extends AppCompatActivity {
                             toast.show();
                             return null;
                         });
-
 
         // When tapping on the AR plane, the corgi will appear!
         arFragment.setOnTapArPlaneListener(
@@ -112,7 +145,109 @@ public class MainActivity extends AppCompatActivity {
                     andy.setRenderable(mCorgi);
                     andy.select();
                 });
+        drawerInit();
 
+        // Find the account type logged in.
+        for (UserInfo user : FirebaseAuth.getInstance().getCurrentUser().getProviderData()) {
+            if (user.getProviderId().equals("facebook.com")) {
+                //For linked FaceBook account
+                Log.d(TAG, "User is signed in with Facebook");
+                mAccountType = "facebook";
+                break;
+            } else if (user.getProviderId().equals("google.com")) {
+                //For linked Google account
+                Log.d(TAG, "User is signed in with Google");
+                mAccountType = "google";
+                break;
+            }
+        }
+
+    }
+
+    private void drawerInit() {
+        mDrawerLayout = findViewById(R.id.drawable_layout);
+        mNavigationView = findViewById(R.id.navigation_view);
+        View navView = mNavigationView.inflateHeaderView(R.layout.navigation_header);
+        mNavProfileImage = navView.findViewById(R.id.profile_image);
+        mProfileName = navView.findViewById(R.id.profile_name);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("My Buddy");
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, mDrawerLayout, mToolbar, R.string.open_drawer, R.string.close_drawer);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    private void buttonInit() {
+        mVoiceFab = findViewById(R.id.voice);
+        //mVoiceFab.setOnClickListener(this);
+
+
+    }
+
+
+
+
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.nav_home: {
+                //Go HOME
+                //Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                //startActivity(intent);
+                //this.overridePendingTransition(0, 0);
+                Toast.makeText(this, "Home Selected!", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            case R.id.nav_settings: {
+                //Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                //startActivity(intent);
+                //SWITCH SETTINGS ACTIVITY
+                Toast.makeText(this, "Settings Selected!", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+            case R.id.nav_logout: {
+                //SIGN OUT
+                signOut();
+                break;
+            }
+        }
+
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mFirebaseAuth.signOut();
+
+        // Google sign out
+        if (mAccountType.equals("google")) {
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
+            mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+            mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                    new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            loadLogInView();
+                        }
+                    });
+//        } else if (mAccountType.equals("facebook")) {
+//            LoginManager.getInstance().logOut();
+//            loadLogInView();
+        } else {
+            loadLogInView();
+        }
     }
 
 }
